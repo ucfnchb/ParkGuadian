5977695fa3128110b95cffde5af23ad1211de850
Yanjun Chen (Stella) <146333771+ucfnchb@users.noreply.github.com>
2024-04-21 14:22:29 +0100

    update display item detail

--- lib/item_detail_page.dart
+++ lib/item_detail_page.dart
@@ -26,22 +26,23 @@ class _ItemDetailPageState extends State<ItemDetailPage> {
         backgroundColor: Colors.white,
         title: const Text('Item Detail', style: TextStyle(fontSize: 14, color: Colors.black, fontWeight: FontWeight.w800),),
       ),
+      // layout and display
       body: Column(children: [
-        _buildLabel('Issue Category:', '${widget.data['email']}'),
+        _buildLabel('Issuer Email:', '${widget.data['email']}'),
         _buildLabel('Latitude:', 'latitude:${widget.data['latitude']}'),
         _buildLabel('Longitude:', 'longitude:${widget.data['longitude']}'),
         _buildLabel('Issue Category:', '${widget.data['category']}'),
         _buildLabel('Issue Description::', '${widget.data['desc']}'),
-        _buildLabel('Email:', '${widget.data['email']}'),
+        _buildLabel('Email Address:', '${widget.data['email']}'),
         const Divider(color: Colors.black, indent: 20, endIndent: 20,),
       ],),
     );
   }
 
   _buildLabel (String title, String content) {
-    return Padding(
+    return Padding(// padding to add spcaing around the label
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-      child: Row(
+      child: Row(//align title
         children: [
           Text(title, style: const TextStyle(fontSize: 14, color: Colors.black),),
           Text(content, style: const TextStyle(fontSize: 14, color: Colors.black),)
